a497f242ffc5ccb24fc914a458a82d298438afad
golemik98 <michal.golemo98@gmail.com>
2021-07-13 13:43:08 +0200

    Correct. Right before logging

--- mqtt/sub_and_http.py
+++ mqtt/sub_and_http.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
+import requests
+
 print()
 
 def on_log(client, userdata, level, buff):
@@ -14,11 +16,32 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, message):
-    print("Topic: ", message.topic, ", ", str(message.payload.decode("utf-8")))
+    # print("Topic: ", message.topic, ", ", str(message.payload.decode("utf-8")))
+    # messages_array.append(str(message.payload.decode("utf-8")))
+    msg = str(message.payload.decode("utf-8"))
+    msg = msg.split(" : ")
+    # timeStamp = msg[0]
+    # sensorName = msg[1]
+    # temperature = msg[2]
+    data_post["timeStamp"] = msg[0]
+    data_post["sensorName"] = msg[1]
+    data_post["temperature"] = msg[2]
+
+    r = requests.post("https://httpbin.org/post", json=data_post)
+    print(r.json()['data'])
+    
 
 
 def on_disconnect(client, userdata, flags, rc=0):
     print("Disconnected result code=", str(rc))
+
+
+#messages_array = []
+data_post = {
+    'timeStamp': 0, 
+    'sensorName': 0, 
+    'temperature': 0
+    }
 
 client = mqtt.Client("P1")
 client.on_connect = on_connect
@@ -29,13 +52,19 @@ client.on_message = on_message
 print("Connecting to the broker ...")
 client.connect("localhost", keepalive=10)
 
-print("Connected!")
 
+#THE LOOP
 client.loop_start()
-
 client.subscribe("dev/temp")
 input(" ")
 client.loop_stop()
+#THE LOOP
+
+
+# for message in messages_array:
+#     print(message)
+
+
 
 client.disconnect()
 
---
+++ strings.py
@@ -0,0 +1,5 @@
+sentence = "Tue Jul 13 10:49:42 2021 : 28-0000061b9d12 : 24.75"
+msg1 = sentence.split(" : ")
+print(msg1)
+
+print(msg1[2])
